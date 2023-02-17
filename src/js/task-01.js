d00ef991816db991a700db717008716119210ed8
Elena Berezina <farfalina1908@gmail.com>
2023-02-17 17:25:50 +0200

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+const categories = document.querySelector("#categories");
+const numOfCategories = countCategories(categories) {
+    let counter = 0;
+    for (let i = 0; i < categories.length; i += 1) {
+        return counter += i;
+    }
+    console.log ("Number of categories: ${counter}");
+}
