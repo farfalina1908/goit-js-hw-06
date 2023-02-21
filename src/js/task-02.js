1e87c99af4f2caadc89e65ba6aba26dc4509f3b6
Elena Berezina <farfalina1908@gmail.com>
2023-02-21 15:54:36 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector("ul#ingredients");
+
+const listItems = ingredients.map(ingredient => {
+const li = document.createElement("li");
+li.textContent = ingredient;
+li.classList.add("item"); 
+return li;
+})
+.reduce((acc, li) => {
+acc.append(li);
+return acc;
+});
+
+list.append(listItems);
