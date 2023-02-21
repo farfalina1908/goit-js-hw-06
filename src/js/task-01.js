433c2fdaaa81db4134aacc4512845aef4041ec74
Elena Berezina <farfalina1908@gmail.com>
2023-02-21 14:23:45 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,8 +1,10 @@
-const categories = document.querySelector("#categories");
-const numOfCategories = countCategories(categories) {
-    let counter = 0;
-    for (let i = 0; i < categories.length; i += 1) {
-        return counter += i;
-    }
-    console.log ("Number of categories: ${counter}");
+const categoriesList = document.querySelector("ul#categories");
+const categoriesItems = categoriesList.querySelectorAll("li.item");
+console.log(`Number og categories: ${categoriesItems.length}`);
+
+for (const item of categoriesItems) {
+    const categoriesName = item.querySelector("h2").textContent;
+    const categoriesElements = item.querySelectorAll("li").length;
+    console.log(`Category: ${categoriesName}`);
+    console.log(`Elements: ${categoriesElements}`);
 }
